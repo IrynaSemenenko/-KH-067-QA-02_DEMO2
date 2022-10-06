c6d934b77343b921f420f4f4389209394f29eca9
Tetiana Huk <tanya.kedruk@gmail.com>
2022-10-07 00:30:58 +0300

    HeaderPage changed

--- PromUa_Automation/src/main/java/HeaderPage.java
+++ PromUa_Automation/src/main/java/HeaderPage.java
@@ -6,18 +6,41 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
-public class HeaderPage extends BasePage{
+public class HeaderPage extends BasePage {
     By singInButtonLocator = By.xpath("//button[@data-qaid='sign-in']");
     By favoriteButton = By.xpath("//button[@data-qaid='favorite_cabinet_button']");
+
+    WebElement myPromButton = driver.findElement(By.xpath("//*[@id='page-block']/div/div[2]/div/div/div[2]/div[2]"));
+
+    WebElement helpButton = driver.findElement(By.xpath("//*[@id='page-block']/div/div[2]/div/div/div[1]/div[3]/a"));
+
+    WebElement promoPanel = driver.findElement(By.xpath("//*[@id='page-block']/div/div[1]"));
+
 
     public HeaderPage(WebDriver driver) {
         super(driver);
     }
+
     public void clickFavoriteButton() {
         wait.until(ExpectedConditions.presenceOfElementLocated(favoriteButton))
                 .click();
     }
+
     public void clickSingInButton() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(singInButtonLocator)).click();
     }
+
+    public void clickHelpButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(helpButton)).click();
+    }
+    public void clickPromoPanel(){
+        wait.until(ExpectedConditions.elementToBeClickable(promoPanel)).click();
+    }
+
+
 }
+
+
+
+
+
