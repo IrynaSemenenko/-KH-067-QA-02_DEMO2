5d5e5fc29f80d1bdafff614775b01c7da5f1bbaf
IrynaSemenenko <iryna.semenenko90@gmail.com>
2022-10-08 23:37:27 +0300

    Added steps to methods, description to tests

--- PromUa_Automation/src/main/java/LoginPage.java
+++ PromUa_Automation/src/main/java/LoginPage.java
@@ -15,30 +15,35 @@ public class LoginPage extends BasePage {
         super(driver);
     }
 
+    @Step("Click on the email button")
     public void clickEmailButton() {
         emailButton = driver.findElement(By.xpath("//div[@data-qaid='email_btn']"));
         wait.until(ExpectedConditions.elementToBeClickable(emailButton))
                 .click();
     }
 
+    @Step("Enter value to the email field")
     public void enterEmail(String email) {
         emailField = driver.findElement(By.id("email_field"));
         wait.until(ExpectedConditions.visibilityOf(emailField))
                 .sendKeys(email);
     }
 
+    @Step("Enter value to the password field")
     public void enterPassword(String password) {
         passwordField = driver.findElement(By.id("enterPassword"));
         wait.until(ExpectedConditions.visibilityOf(passwordField))
                 .sendKeys(password);
     }
 
+    @Step("Click on the email button")
     public void clickSubmitEmailButton() {
         submitEmailButton = driver.findElement(By.id("emailConfirmButton"));
         wait.until(ExpectedConditions.elementToBeClickable(submitEmailButton))
                 .click();
     }
 
+    @Step("Click on the password button")
     public void clickSubmitPasswordButton() {
         submitPasswordButton = driver.findElement(By.id("enterPasswordConfirmButton"));
         wait.until(ExpectedConditions.elementToBeClickable(submitPasswordButton))
