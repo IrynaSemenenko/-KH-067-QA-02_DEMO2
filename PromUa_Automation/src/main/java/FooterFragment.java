4e7ec8ea606824e9c5d7ef6f57115f9e4b7b3acc
Viktoriia Khatniuk <hatnukvika@gmail.com>
2022-10-08 22:22:03 +0300

    Footer tests updated

--- PromUa_Automation/src/main/java/FooterFragment.java
+++ PromUa_Automation/src/main/java/FooterFragment.java
@@ -7,6 +7,7 @@ public class FooterFragment extends BasePage{
     WebElement buyersHelpLink = driver.findElement(new By.ByXPath("//a[@href='https://help.prom.ua/hc/ru']"));
     WebElement sellersHelpLink = driver.findElement(new By.ByXPath("//a[@href='https://support.prom.ua/hc/ru']"));
     WebElement tariffsLink = driver.findElement(new By.ByXPath("//a[@href='https://my.prom.ua/ua/how_to_order']"));
+    WebElement userAgreement = driver.findElement(new By.ByXPath( "//a[@href='/ua/terms-of-use']"));
 
     public FooterFragment(WebDriver driver) {
         super(driver);
@@ -21,4 +22,5 @@ public class FooterFragment extends BasePage{
     public void clickTariffsLink() {
         wait.until(ExpectedConditions.visibilityOf(tariffsLink)).click();
     }
+    public void clickUserAgreement(){wait.until(ExpectedConditions.visibilityOf(userAgreement)).click();}
 }
