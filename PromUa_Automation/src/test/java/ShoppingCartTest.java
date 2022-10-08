22ea946ad8ef006f7597395a22fa99496cc89df6
Viktor Deinyi <deinyiviktor@gmail.com>
2022-10-08 22:45:52 +0300

    Shopping cart changes

--- PromUa_Automation/src/test/java/ShoppingCartTest.java
+++ PromUa_Automation/src/test/java/ShoppingCartTest.java
@@ -3,18 +3,16 @@ import org.testng.annotations.Test;
 
 public class ShoppingCartTest extends BaseTest {
     HomePage homePage;
-    HeaderTest headerPage;
     ShoppingCartPage shoppingCartPage;
 
     @Test
     public void testDisabledMinusButton() {
         homePage = new HomePage(driver);
-        headerPage = new HeaderTest(driver);
         shoppingCartPage = new ShoppingCartPage(driver);
         //scroll to often chosen elements block
         homePage.clickProduct();
         //wait shopping cart button and click on it
-        headerPage.clickShoppingCartButton();
+        homePage.getHeaderFragment().clickShoppingCartButton();
         //check decrease button
         Assert.assertFalse(driver.findElement(shoppingCartPage.decreaseProductAmountButton).isEnabled());
     }
@@ -22,12 +20,11 @@ public class ShoppingCartTest extends BaseTest {
     @Test
     public void testEnabledMinusButton() {
         homePage = new HomePage(driver);
-        headerPage = new HeaderTest(driver);
         shoppingCartPage = new ShoppingCartPage(driver);
         //scroll to often chosen elements block
         homePage.clickProduct();
         //wait shopping cart button and click on it
-        headerPage.clickShoppingCartButton();
+        homePage.getHeaderFragment().clickShoppingCartButton();
         //increase amount of product
         shoppingCartPage.increaseProductAmount();
         //check decrease button
