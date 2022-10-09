181205277976999b8bedd8805dda9fa81611084c
NataKhakhlina <nata.khakhllina@gmail.com>
2022-10-09 13:00:56 +0300

    Additional functional test on main page

--- PromUa_Automation/src/main/java/HomePage.java
+++ PromUa_Automation/src/main/java/HomePage.java
@@ -13,8 +13,19 @@ public class HomePage extends BasePage {
     By product = By.xpath("(//a[@data-qaid='buy-button'])[1]");
     By titleForYou = By.xpath("(//div[@class='M3v0L YKUY6'])[2]");
 
+    By searchButtonLocator = By.xpath("//*[@data-qaid='search_btn']");
+    By ruLanguageLocator = By.xpath("//*[@data-qaid='ru_lang']");
+    By uaLanguageLocator = By.xpath("//*[@data-qaid='ua_lang']");
+    By menuProductsLocator = By.xpath("//*[@data-qaid='menu_preview']");
+    By seasonFeedLocator = By.xpath("//*[@data-qaid='menu_parent']");
+
     public HomePage(WebDriver driver) {
         super(driver);
+    }
+
+    public void clickLanguageButton(By languageLocator) {
+        WebElement languageButton = driver.findElement(languageLocator);
+        languageButton.click();
     }
 
     public void clickFavoriteButtons() {
